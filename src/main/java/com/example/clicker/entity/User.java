c1e535ef5951114ed254737ea6b8fb63a4bd5534
Zinko17 <vovazin2@gmail.com>
2025-11-05 17:30:29 +0600

    ентити для юзера

---
+++ src/main/java/com/example/clicker/entity/User.java
@@ -0,0 +1,36 @@
+package com.example.clicker.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.management.relation.Role;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER,
+        ADMIN,
+    }
+}
